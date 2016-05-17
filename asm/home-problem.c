73df9ff99f4f5efb6dd19efadf338b699a4a3501
kirapangzi <kirapangzi@qq.com>
2016-05-17 10:13:01 +0800

    Chapter asm all done

--- asm/home-problem.c
+++ asm/home-problem.c
@@ -1,11 +1,150 @@
 #include <stdio.h>
 
 
-//3.60
+typedef struct ELE *tree_ptr;
 
+struct ELE {
+	long val;
+	tree_ptr left;
+	tree_ptr right;
+};
+
+//3.70
+long traverse(tree_ptr tp)
+{
+	long z = -9223372036854775808;
+	if (!tp)
+		return z;
+	long x = tp->val; //rbx
+	tp = tp->left;
+	long y = traverse(tp);// r12
+	tp = tp->right;
+	z = traverse(tp);// rax
+	z = ((z >= y)? z : y);
+	return (z < x)? x : z;	
+}
+// traverse tp in rdi
+// save rbx in rsp - 24
+// rbp in rsp -16
+// r12 in rsp -8
+// alloc rsp -24
+// rbp = rdi = tp
+// rax = $value -9223372036854775808
+// test tp
+// if == 0 goto L9
+// rbx = tp->val
+// rdi p = tp->left
+// call traverse
+// r12 = rax
+// rdi p = tp->right
+// call traverse
+// r12 : rax
+// if r12>=rax rax = r12
+// rax : rbx
+// if rax < rbx rax = rbx
+// L9
+// restore rbx,rbp,r12
+// dealloc rsp
+// return
 
 #if 0
+//3.69
+//trace tp in rdi
+// eax = 0
+// test tp
+// if == 0 goto L3
+// L5
+// rax = *tp
+// ele = (rdi + 16) /ele.right
+// test rdi
+// if !=0 goto L5
+// L3
+// return rax
 
+//get the most right leaf value of the tree
+long trace(tree_ptr tp)
+{
+	long result = 0;
+	if (!tp)
+		return 0;
+	//result = tp->val;
+	while (tp->right)
+		tp = tp->right;
+	return tp->val;
+}
+
+//3.68
+int good_echo()
+{
+	char buf[4096] = {0};
+	fgets((void *)buf, sizeof(buf), stdin);
+	puts((void *)buf);
+}
+
+int main()
+{
+	good_echo();
+}
+
+
+//3.67
+union ele {
+	struct {
+		int *p;
+		int y;
+	} e1;
+	struct {
+		int x;
+		union ele *next;
+	} e2;
+};
+void proc (union ele *up)
+{
+     up->e2.next->e1.y= *(up->e2.next->e1.p)-up->e2.x;
+}
+
+//3.62
+typedef int ** Marray_t;
+#define M 10
+
+void transpose(Marray_t A)
+{
+	int i, j;
+	for (i = 0; i < M; i++)
+		for (j = 0; j < i; j++) {
+			int t = A[i][j];
+			A[i][j] = A[j][i];
+			A[j][i] = t;
+		}
+}
+
+//3.61
+#if 1
+int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k)
+{
+   int j;
+   int result = 0;
+   int *ap=A[i];
+   int *bp=&B[0][k];
+   for(j = 0; j < n; j++)
+   {
+	   result += *ap * *bp;
+	   ap++;
+	   bp+=n;
+   }
+	return result;
+}
+#else
+int var_prod_ele(int n, int A[n][n], int B[n][n], int i, int k) {
+	int j;
+	int result = 0;
+	for (j = 0; j < n; j++)
+		result += A[i][j] * B[j][k];
+
+	return result;
+}
+
+#endif
 
 //3.59
 //save ebp
@@ -44,7 +183,6 @@ int switch_prob(int x, int n)
 	}
 	return result;
 }
-
 
 //3.58
 //L17 E
@@ -111,7 +249,6 @@ int cread_alt(int *xp)
 	rp = xp ? xp : &result;
 	return *rp;
 }
-
 
 //3.56
 // esi = x
